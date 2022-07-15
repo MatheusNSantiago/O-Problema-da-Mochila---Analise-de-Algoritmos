cdbacf4e4ecce824bd93f7f51c6e4cd30612cfa4
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-07-14 22:32:09 -0300

    final

--- utils.py
+++ utils.py
@@ -3,24 +3,25 @@ from tabulate import tabulate
 from time import time
 
 
-def make_table(results: dict[int, list]):
+def make_table(results: dict[int, list], format="fancy_grid"):
     # Adiciona a média de cada categoria
     for valores_cat in zip(*results.values()):
         media_valores_cat = mean(valores_cat)
 
-        results.setdefault("Média", []).append(media_valores_cat)
+        results.setdefault("Média", []).append(round(media_valores_cat, 3))
 
     # Adiciona a média de cada sample size
     for values in results.values():
-        values.append(mean(values))
+        values.append(round(mean(values), 3))
 
-    return tabulate(
+    print(
+        tabulate(
             {" ": ["Categoria 1", "Categoria 2", "Categoria 3", "Média"]} | results,
             headers="keys",
             numalign="center",
-            tablefmt="latex",
+            tablefmt=format,
         )
-    
+    )
 
 
 def fazer_testes(funcao, repeticoes, sizes=[100, 200, 500, 1000, 2000, 5000, 10000]):
@@ -35,14 +36,14 @@ def fazer_testes(funcao, repeticoes, sizes=[100, 200, 500, 1000, 2000, 5000, 100
                 end = time()
 
                 if i == 0:
-                    res.setdefault(size, []).append(desempenho)
+                    res.setdefault(size, []).append(round(desempenho, 3))
 
                 # Guarda o tempo de execução dos 3 maiores sample sizes para cada categoria
-               
+
                 t = end - start
 
                 tempos.setdefault(size, [0, 0, 0])
-                tempos[size][cat - 1] = t
+                tempos[size][cat - 1] = round(t, 3)
 
     for ts in tempos.values():
         for t in ts:
